de6734ffcbbb5e1daa600fdde0b3ef63cce7dfd4
caru-ini <s1F102400392@iniad.org>
2024-05-07 21:42:28 +0900

    Add basic allign of minesweeper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,45 @@
-import styles from './index.module.css';
+import styles from './index.module.scss';
+import { useState } from 'react';
 
 const Home = () => {
+  const [samplePos, setSamplePos] = useState(0);
+  const [bombMap, setBombMap] = useState<(0 | 1)[][]>([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const [userInputs, setuserInputs] = useState<(0 | 1 | 2 | 3)[]>([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+  const board = structuredClone(bombMap);
+
+  console.log('sample', samplePos);
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            pages/index.js
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div className={styles.board} style={{ backgroundPosition: `${-30 * samplePos}px 0px` }}>
+        {board.map((row, rowIndex) => (
+          <div key={rowIndex} className={styles.row}>
+            {row.map((cell, cellIndex) => (
+              <div
+                key={cellIndex}
+                className={styles.cell}
+                onClick={() => {
+                  const newBombMap = structuredClone(bombMap);
+                  newBombMap[rowIndex][cellIndex] = 1;
+                  setBombMap(newBombMap);
+                }}
+              >
+                {cell}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
