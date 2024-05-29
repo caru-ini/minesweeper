b9cc088f00fd07eb2c76d140ad52e310b6605210
caru-ini <s1F102400392@iniad.org>
2024-05-29 17:42:50 +0900

    feat: Add web font

--- src/components/topArea.tsx
+++ src/components/topArea.tsx
@@ -1,6 +1,9 @@
 import type { gameStatusType } from '../hooks/game';
+import localFont from 'next/font/local';
 
 import styles from '../pages/index.module.css';
+
+const font = localFont({ src: './../pages/DSEG7Classic-Bold.woff2' });
 
 interface TopAreaProps {
   bombCount: number;
@@ -11,13 +14,17 @@ interface TopAreaProps {
 export const TopArea: React.FC<TopAreaProps> = ({ bombCount, gameStatus, resetGame }) => {
   return (
     <div className={styles.menu}>
-      <div className={styles.ndisp}>{bombCount.toString().padStart(3, '0')}</div>
+      <div className={`${styles.ndisp} ${font.className}`}>
+        {bombCount.toString().padStart(3, '0')}
+      </div>
       <div
         className={styles.smiley}
         onClick={resetGame}
         style={{ backgroundPositionX: ['-330px', '-360px', '-390px'][gameStatus.status] }}
       />
-      <div className={styles.ndisp}>{gameStatus.time.toString().padStart(3, '0')}</div>
+      <div className={`${styles.ndisp} ${font.className}`}>
+        {gameStatus.time.toString().padStart(3, '0')}
+      </div>
     </div>
   );
 };
